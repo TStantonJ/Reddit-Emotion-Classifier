dd5be314b0f9eff625f90045d6a5033572e0e799
ThomasRStanton <thomasrstantonjr@gmail.com>
2023-12-02 18:00:04 -0500

    Usability Tweek

--- streamlitDisplay.py
+++ streamlitDisplay.py
@@ -1,12 +1,22 @@
 import streamlit as st
-from utils_app import side_bar, tabs
+from utils_app import side_bar, data_tabs, model_tab, analysis_tab
 # %%--------------------------------------------------------------------------------------------------------------------
 st.title("Streamlit App Testing")
 # %%--------------------------------------------------------------------------------------------------------------------
+# Session variables and utility function instantiation
+st.session_state.dataSource = "None Selected"
 side_bar()
-tabs()
-st.write('This is the place you can start writing')
+
+# Data Section
+data_tabs()
 st.divider()
+
+# Model Section
+model_tab()
+st.divider()
+
+# Analysis Section
+analysis_tab()
 # %%--------------------------------------------------------------------------------------------------------------------
 code = """df = pd.DataFrame(
     np.random.randint(low=0, high=100, size=(10, 10)),
--- utils_app.py
+++ utils_app.py
@@ -9,64 +9,39 @@ def side_bar():
             # Clear the chat history
             st.session_state.messages = []
 
-def tabs():
-    tab1, tab2, tab3 = st.tabs(["🗃 Data", " Model", " Analysis"])
-    with tab1:
-        data_tab()
+# Function taht controls the data selection tabs
+def data_tabs():
+    with st.expander("Data",expanded=True):
+        st.subheader("Data Sources")
+        tab1, tab2= st.tabs(["Pre-Loaded Data", " Live Data",])
+        with tab1:
+            # Source select for preloaded data
+            dataSource = pd.DataFrame()
+            dataOption_selectbox = st.selectbox(
+            'Select Ddta from Pre-Loaded sources',
+            ('Reddit Source A', 'Reddit Source B', 'Reddit Source C'),
+            index=None,
+            placeholder="Select data source...",)
+
+            if dataOption_selectbox == 'Reddit Source A':
+                dataSource = pd.read_csv('preloadedData/merged_reddit_data.csv')
+                st.session_state.dataSource = dataSource
+            elif dataOption_selectbox == 'Reddit Source B':
+                pass
+            elif dataOption_selectbox == 'Reddit Source C':
+                pass
+            
+            # Display of selected data
+            st.dataframe(dataSource, use_container_width=True)
         
     with tab2:
         st.subheader("A tab with the data")
-    with tab3:
-        st.subheader("A tab with the results")
 
-def data_tab():
-    st.subheader("Data Sources")
-    
-    # Toggle to select whether preloaded data is used or data is gotten life
-    dataMode_toggle = st.toggle('Pre-Loaded Data mode')
-    if dataMode_toggle:
-        
-        # Options for preloaded data
-        dataSource = pd.DataFrame()
-        dataOption_selectbox = st.selectbox(
-        'Select Ddta from Pre-Loaded sources',
-        ('Reddit Source A', 'Reddit Source B', 'Reddit Source C'),
-        index=None,
-        placeholder="Select data source...",)
+def model_tab():
+    st.subheader("Model Sources")
 
-        if dataOption_selectbox == 'Reddit Source A':
-            st.write('Set')
-            dataSource = pd.read_csv('preloadedData/merged_reddit_data.csv')
-            st.session_state.dataSource = dataSource
-        elif dataOption_selectbox == 'Reddit Source B':
-            pass
-        elif dataOption_selectbox == 'Reddit Source C':
-            pass
-
-        st.dataframe(dataSource, use_container_width=True)
-    else: 
-        st.write('Select Supported SubReddit')
-
-    container_2 = st.empty()
-    button_A = container_2.button('Btn A')
-    if button_A:
-        container_2.empty()
-        button_B = container_2.button('Btn B')
-    uploaded_file = st.file_uploader("Choose a file")
-    print(uploaded_file)
-    if uploaded_file is not None:
-        st.write("You selected the file:", uploaded_file.name)
-        st.session_state.data = uploaded_file
-
-    st.session_state.data = ""
-    if st.button('Say hello'):
-        filenames = os.listdir('.')
-        selected_filename = st.selectbox('Select a file', filenames)
-        file_path = os.path.join('.', selected_filename)
-        st.session_state.data = file_path
-        st.write('Goodbye')
-    else:
-        st.write('Goodbye')
+def analysis_tab():
+    st.subheader("Analysis")
 
 def file_selector(folder_path='.'):
     filenames = os.listdir(folder_path)
