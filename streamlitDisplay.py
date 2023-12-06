f74c005b63abc1c007a2d41b2463b11b68a4f79b
ThomasRStanton <thomasrstantonjr@gmail.com>
2023-12-06 18:24:18 -0500

    comit

--- streamlitDisplay.py
+++ streamlitDisplay.py
@@ -1,28 +1,32 @@
 import streamlit as st
 from utils_app import side_bar, data_tabs, model_tab, analysis_tab
 # %%--------------------------------------------------------------------------------------------------------------------
-st.title("Streamlit App Testing")
-# %%--------------------------------------------------------------------------------------------------------------------
-# Session variables and utility function instantiation
-st.session_state.dataSource = "None Selected"
-side_bar()
+mainTab1, mainTab2= st.tabs(["Sentiment Training", "Sentiment Logging Training",])
+with mainTab1:
+    st.title("Sentiment Training")
+    # %%--------------------------------------------------------------------------------------------------------------------
+    # Session variables and utility function instantiation
+    st.session_state.dataSource = "None Selected"
+    side_bar()
 
-# Data Section
-data_tabs()
-st.divider()
+    # Data Section
+    data_tabs()
+    st.divider()
 
-# Model Section
-model_tab()
-st.divider()
+    # Model Section
+    model_tab()
+    st.divider()
 
-# Analysis Section
-analysis_tab()
-# %%--------------------------------------------------------------------------------------------------------------------
-code = """df = pd.DataFrame(
-    np.random.randint(low=0, high=100, size=(10, 10)),
-    columns=('col %d' % i for i in range(10)))
+    # Analysis Section
+    analysis_tab()
+    # %%--------------------------------------------------------------------------------------------------------------------
+    code = """df = pd.DataFrame(
+        np.random.randint(low=0, high=100, size=(10, 10)),
+        columns=('col %d' % i for i in range(10)))
 
-st.dataframe(df)"""
-st.code(code, language="python")
+    st.dataframe(df)"""
+    st.code(code, language="python")
 
-print('Amir')
+    print('Amir')
+with mainTab2:
+    st.title("Sentiment Logging Training")
