52fdb6dc19b7eba48e62a7071e718fc648a06530
ThomasRStanton <thomasrstantonjr@gmail.com>
2023-12-09 14:35:20 -0500

    Aws runnable

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from streamlit.web import cli as stcli
 
 
 if __name__ == '__main__':
-    sys.argv = ["streamlit", "run", "streamlitDisplay.py", "Usr"]
+    sys.argv = ["streamlit", "run", "streamlitDisplay.py", "Usr", "--server.port", "8888" ]
     sys.exit(stcli.main())
