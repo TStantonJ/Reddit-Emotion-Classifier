0841ca8aaa24ad4ec3de7150b45615952b689aa4
ThomasRStanton <thomasrstantonjr@gmail.com>
2023-12-11 16:29:23 -0500

    Breakout reddit scrape

--- analysis_page.py
+++ analysis_page.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import streamlit as st
+
 from collections import Counter
-from datetime import datetime, timedelta
-from concurrent.futures import ThreadPoolExecutor
 from transformers import pipeline
 from Model_ForGIT6_model_apply import*
+from reddit_scraper import reddit_scraper
 
 def preprocess_text(self, text):
     
@@ -230,90 +230,3 @@ def analysis_model_tab():
         # Display the plot in Streamlit
         st.pyplot(fig)
 
-
-def reddit_scraper(client_id, client_secret, user_agent, num_posts, subreddit_name, interval, time_filter, top_comments_count, output_file):
-    class RedditScraper:
-        def __init__(self, client_id, client_secret, user_agent):
-            self.reddit = praw.Reddit(
-                client_id=client_id,
-                client_secret=client_secret,
-                user_agent=user_agent)
-
-        def fetch_posts(self, num_posts, sub_name, interval):
-            subreddit = self.reddit.subreddit(sub_name)
-            print(time_filter)
-            posts = subreddit.top(time_filter=str(time_filter), limit=num_posts)
-            posts_list = list(posts)
-            posts_list.sort(key=lambda post: post.created_utc, reverse=True)
-
-            intervals = {
-                'daily': timedelta(days=1),
-                'weekly': timedelta(weeks=1),
-                'monthly': timedelta(weeks=4)}
-
-            end_time = datetime.utcfromtimestamp(posts_list[0].created_utc)
-            nested_posts = []
-            current_interval_start = end_time
-            data = []
-            interval_num = 0
-
-            for post in posts_list:
-                post_time = datetime.utcfromtimestamp(post.created_utc)
-
-                if post_time < current_interval_start - intervals[interval]:
-                    interval_num += 1
-                    current_interval_start = post_time
-
-                data.append({
-                    'Post/Comment': 'Post',
-                    'ID': post.id,
-                    'Text': post.title + post.selftext,
-                    'Creation Date': datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d'),
-                    'Interval Number': interval_num})
-
-            interval_counts = Counter([entry['Interval Number'] for entry in data])
-            plt.bar(interval_counts.keys(), interval_counts.values())
-            plt.xlabel('Interval Number')
-            plt.ylabel('Number of Entries')
-            plt.title('Number of Entries per Interval')
-            plt.show()
-
-            return data, posts_list
-
-        def fetch_comments(self, submission, limit, interval_num):
-            submission.comment_sort = 'best'
-            submission.comments.replace_more(limit=0)
-
-            return [{'Post/Comment': 'Comment', 'ID': submission.id, 'Text': comment.body,
-                     'Creation Date': datetime.utcfromtimestamp(comment.created_utc).strftime('%Y-%m-%d'),
-                     'Interval Number': interval_num} for comment in submission.comments.list()[:limit]]
-
-        def create(self, num_posts, subreddit_name, interval, top_comments_count, output_file):
-
-            data, posts_list = self.fetch_posts(num_posts, subreddit_name, interval)
-            interval_nums = [d['Interval Number'] for d in data]
-
-            with ThreadPoolExecutor() as executor:
-                comments_list = list(executor.map(lambda p: self.fetch_comments(p[0], top_comments_count, p[1]),
-                                                  list(zip(posts_list, interval_nums))))
-
-            data.extend([comment for comment_list in comments_list for comment in comment_list])
-
-            df = pd.DataFrame(data)
-            #df.to_csv(output_file, index=True)
-            return df
-
-    scraper = RedditScraper(client_id, client_secret, user_agent)
-    tmp = scraper.create(num_posts, subreddit_name, interval, top_comments_count, output_file)
-    return tmp
-
-
-def plot_sentiment_scores(average_scores):
-    plt.figure(figsize=(10, 6))
-    plt.bar(range(len(average_scores)), average_scores, color='skyblue')
-    plt.xlabel('Interval Number')
-    plt.ylabel('Average Sentiment Score')
-    plt.title('Average Sentiment Scores per Interval')
-    plt.xticks(range(len(average_scores)))
-    plt.ylim([0, 1])
-    return plt
