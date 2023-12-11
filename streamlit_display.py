a04155bf38473ca6593dceeb4bdfcf995bc60bcc
ThomasRStanton <thomasrstantonjr@gmail.com>
2023-12-11 17:39:56 -0500

    githubs fault

--- streamlit_display.py
+++ streamlit_display.py
@@ -1,10 +1,8 @@
 import streamlit as st
 from training_page import  train_data_tabs, train_model_tab, train_analysis_tab
 from analysis_page import analysis_data_tabs, analysis_model_tab
-<<<<<<< Updated upstream:streamlitDisplay.py
-# %%--------------------------------------------------------------------------------------------------------------------
+
 mainTab1, mainTab2= st.tabs([ "Sentiment Logging Training", "Sentiment Training",])
-=======
 
 """
 Main function calls of the project.
@@ -14,7 +12,6 @@ mainTab2: Allows for demonstraition of models and training of new bert models.
 mainTab1, mainTab2= st.tabs([ "Sentiment Logging", "Model Training & Analysis",])
 
 # Main analysis/application page
->>>>>>> Stashed changes:streamlit_display.py
 with mainTab1:
     st.title("Sentiment Logging Training")
     analysis_data_tabs()
@@ -22,13 +19,7 @@ with mainTab1:
 
 # Model verification/training page
 with mainTab2:
-<<<<<<< Updated upstream:streamlitDisplay.py
-    st.title("Sentiment Training of models")
-    # %%--------------------------------------------------------------------------------------------------------------------
-    # Session variables and utility function instantiation
-=======
     st.title("Model Training & Analysis")
->>>>>>> Stashed changes:streamlit_display.py
     st.session_state.dataSource = "None Selected"
 
     # Data Section
@@ -39,12 +30,5 @@ with mainTab2:
     train_model_tab()
     st.divider()
 
-<<<<<<< Updated upstream:streamlitDisplay.py
     # Analysis Section
     train_analysis_tab()
-
-
-
-# %%
-=======
->>>>>>> Stashed changes:streamlit_display.py
